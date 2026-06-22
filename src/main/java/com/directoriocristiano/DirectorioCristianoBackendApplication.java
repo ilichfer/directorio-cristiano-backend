f5dd44be32fc9526ffdded11cf69a5250ea923e2
ilichfer <47036020+ilichfer@users.noreply.github.com>
2026-06-22 16:42:56 -0500

    create project

---
+++ src/main/java/com/directoriocristiano/DirectorioCristianoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.directoriocristiano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorioCristianoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorioCristianoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/directoriocristiano/DirectorioCristianoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.directoriocristiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorioCristianoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
