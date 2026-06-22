206f8cecf744cd9b862d6b2d36af05b2bf90b48c
ilichfer <47036020+ilichfer@users.noreply.github.com>
2026-06-22 17:52:14 -0500

    add configuratios
    add configuratios

---
+++ src/main/java/com/directoriocristiano/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.directoriocristiano.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Directorio Cristiano Local API")
+                        .version("1.0.0")
+                        .description("API para la plataforma Directorio Cristiano Local"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
